edc2db04cd55b647aca17d38172f7b328e640594
Maxime CHAMPIGNEUX <contact@maximechampigneux.fr>
2025-01-07 22:24:53 +0100

    project ended

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,13 +1,11 @@
 import api from '../data/data.json';
 
-const logements = api.map(({ id, title, cover }) => ({
+export const logements = api.map(({ id, title, cover }) => ({
 	id,
 	title,
 	cover,
 }));
 
-const getSingleLogementsById = (id) => {
+export const getSingleLogementsById = (id) => {
 	return api.find((data) => data.id === id);
 };
-
-export { logements, getSingleLogementsById };
